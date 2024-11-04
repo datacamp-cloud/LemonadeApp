69c6e8095fe7e3baa7203ad1868cc33f5819cae7
datacamp-cloud <>
2024-11-04 00:33:29 +0000

    end of lemonade app's

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -25,8 +26,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,7 +50,8 @@ fun LemonadeApp(modifier: Modifier = Modifier) {
 
     var showImage by remember { mutableStateOf("tree") }
 
-    //val showImage = nextImage()
+    var clicksRequired by remember { mutableIntStateOf((2..5).random()) }
+    var clicksDone by remember { mutableIntStateOf(0) }
 
     val nextImage: () -> String = {
         when (showImage) {
@@ -78,9 +83,34 @@ fun LemonadeApp(modifier: Modifier = Modifier) {
 
     Column (
         horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "Lemonade",
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            fontSize = 18.sp,
+            modifier = modifier
+                .background(
+                    color = Color.Yellow
+                )
+                .padding(
+                    top = 60.dp,
+                    bottom = 0.dp,
+                    start = 156.dp,
+                    end = 156.dp
+                )
+        )
+    }
+
+
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = modifier.fillMaxSize()
     ){
+
         Image(
             painter = painterResource(id = imageResource),
             contentDescription = "",
@@ -90,7 +120,20 @@ fun LemonadeApp(modifier: Modifier = Modifier) {
                     shape = RoundedCornerShape(12.dp)
                 )
                 .padding(35.dp)
-                .clickable { showImage = nextImage() } //modifie directement l'image en fonction de l'image selectionne
+                .clickable {
+                    if (showImage == "press"){
+                        clicksDone++
+
+                        if (clicksDone >= clicksRequired){
+                            showImage = nextImage()
+                            clicksDone = 0
+                            clicksRequired = (2..5).random()
+                        }else{
+                            showImage = nextImage()
+                        }
+                    }
+                    showImage = nextImage()
+                } //modifie directement l'image en fonction de l'image selectionne
         )
 
         Spacer(
